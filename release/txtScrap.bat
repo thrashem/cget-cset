efc2663d90637b8c68346d57859910e45fe98504
thrashem <thrash@thrashem.com>
2025-12-03 06:05:06 +0900

    #Update

--- release/txtScrap.bat
+++ release/txtScrap.bat
@@ -1,12 +1,30 @@
-@ECHO OFF
+@echo off
 CHCP 65001 >nul
-SET SAVE_DIR=G:\マイドライブ\txtScrap\
-IF NOT EXIST "%SAVE_DIR%" MKDIR "%SAVE_DIR%"
-FOR /F "tokens=1-3 delims=/ " %%a IN ("%DATE%") DO SET YYYYMMDD=%%a%%b%%c
-ECHO ## %DATE% %time:~0,8% >> "%SAVE_DIR%%YYYYMMDD%.md"
-ECHO. >> "%SAVE_DIR%%YYYYMMDD%.md"
-.\cget >> "%SAVE_DIR%%YYYYMMDD%.md"
-ECHO. >> "%SAVE_DIR%%YYYYMMDD%.md"
-ECHO *** >> "%SAVE_DIR%%YYYYMMDD%.md"
-ECHO. >> "%SAVE_DIR%%YYYYMMDD%.md"
-IF %ERRORLEVEL% NEQ 0 msg "%USERNAME%" "保存に失敗しました"
+setlocal enabledelayedexpansion
+
+:: 保存先とファイル名の準備
+set "SAVE_DIR=G:\マイドライブ\txtScrap\"
+if not exist "%SAVE_DIR%" mkdir "%SAVE_DIR%"
+
+for /f "tokens=1-3 delims=/ " %%a in ("%DATE%") do set "YYYYMMDD=%%a%%b%%c"
+set "OUTFILE=%SAVE_DIR%%YYYYMMDD%.md"
+
+:: 一時ファイルにMarkdownを出力
+ECHO ## %DATE% %time:~0,8% > temp.txt
+ECHO. >> temp.txt
+.\cget >> temp.txt
+if %ERRORLEVEL% EQU 1 (
+    msg "%USERNAME%" "Cannot open clipboard (code 1)"
+    goto :EOF
+)
+if %ERRORLEVEL% EQU 2 (
+    msg "%USERNAME%" "No text data (code 2)"
+    goto :EOF
+)
+ECHO. >> temp.txt
+ECHO *** >> temp.txt
+ECHO. >> temp.txt
+
+:: Markdownファイルに追記
+TYPE .\temp.txt >> "%OUTFILE%"
+endlocal
